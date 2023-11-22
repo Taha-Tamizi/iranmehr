d541796247dd2a71516702f86e1e6c9cb4ed71a1
taha tamizi <t-tamizi@IMI.COM>
2023-11-22 12:05:52 +0330

    changed some styles

--- assets/main/index.js
+++ assets/main/index.js
@@ -41,19 +41,19 @@ jQuery(document).ready(function ($) {
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 3,
+          slidesToShow: 2,
           slidesToScroll: 1,
         },
       },
       {
-        breakpoint: 600,
+        breakpoint: 768,
         settings: {
           slidesToShow: 2,
           slidesToScroll: 1,
         },
       },
       {
-        breakpoint: 480,
+        breakpoint: 625,
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1,
